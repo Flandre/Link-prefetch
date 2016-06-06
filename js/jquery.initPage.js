1023858e1991006f1d4f5f6604d20e0c19706b8a
Flandre <192488668@qq.com>
2016-06-06 14:00:08 +0800

    add pagehash

--- js/jquery.initPage.js
+++ js/jquery.initPage.js
@@ -19,19 +19,19 @@ function nextPage() {
   if (currentPage + 1 < totalPage) {
     currentPage++;
   }
-  showCurrentPage(currentPage, totalPage)
+  showCurrentPage(currentPage)
 }
 //向上翻页
 function prevPage() {
-  var totalPage = $('section', 'main').length;
   if (currentPage > 0) {
     currentPage--;
   }
-  showCurrentPage(currentPage, totalPage)
+  showCurrentPage(currentPage)
 }
 
 //显示当前页面
-function showCurrentPage(current, total) {
+function showCurrentPage(current) {
+  var total = $('section', 'main').length;
   $('section', 'main').removeClass('active');
   $('section:eq(' + current + ')', 'main').addClass('active');
   //判断还有没有下一页,如果有则加载下一页
@@ -45,10 +45,15 @@ function fetchPage(next) {
   console.log(next);
   var nextPage = $('section:eq(' + next + ')', 'main'),
   url = nextPage.attr('data-dom');
-  getSource(url, 'html', function(data){
-    console.log(data);
-    nextPage.html(data)
-  })
+  //判断页面有没有加载过
+  if(!pageHash[url]){
+    getSource(url, 'html', function(data){
+      console.log(data);
+      nextPage.html(data)
+    });
+    pageHash[url] = true;
+    console.log(pageHash)
+  }
 }
 
 //发起ajax请求
