fe61af9399822a516ea19b7ab4768d396c822e30
Flandre <192488668@qq.com>
2016-06-06 13:52:37 +0800

    追加翻页

--- js/jquery.initPage.js
+++ js/jquery.initPage.js
@@ -5,29 +5,36 @@ var currentPage = 0;
 function initFirstPage() {
   //处理加载第一页
   var totalPage = $('section', 'main').length;
-  alert(totalPage);
-  fetchPage(-1);
+  fetchPage(currentPage);
   showCurrentPage(currentPage, totalPage);
   //添加向下翻页
-  $('#nextBtn').on('click', nextPage());
-  $('#prevBtn').on('click', prevPage());
+  $('#nextBtn').on('click', nextPage);
+  $('#prevBtn').on('click', prevPage);
 }
 
 /*翻页事件*/
 //向下翻页
 function nextPage() {
-
+  var totalPage = $('section', 'main').length;
+  if (currentPage + 1 < totalPage) {
+    currentPage++;
+  }
+  showCurrentPage(currentPage, totalPage)
 }
 //向上翻页
 function prevPage() {
-
+  var totalPage = $('section', 'main').length;
+  if (currentPage > 0) {
+    currentPage--;
+  }
+  showCurrentPage(currentPage, totalPage)
 }
 
 //显示当前页面
 function showCurrentPage(current, total) {
-  //code
-
-  //判断还有没有下一页
+  $('section', 'main').removeClass('active');
+  $('section:eq(' + current + ')', 'main').addClass('active');
+  //判断还有没有下一页,如果有则加载下一页
   if (current + 1 < total) {
     fetchPage(current + 1)
   }
@@ -35,7 +42,22 @@ function showCurrentPage(current, total) {
 
 //预加载下一页
 function fetchPage(next) {
+  console.log(next);
+  var nextPage = $('section:eq(' + next + ')', 'main'),
+  url = nextPage.attr('data-dom');
+  getSource(url, 'html', function(data){
+    console.log(data);
+    nextPage.html(data)
+  })
+}
 
+//发起ajax请求
+function getSource(url, type, func) {
+  $.get({
+    url: url,
+    dataType: type,
+    success: func
+  });
 }
 
 initFirstPage();
