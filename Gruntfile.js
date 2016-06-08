ee2e8b4502d02b3c178148569b4c9c28044e6dad
Flandre <192488668@qq.com>
2016-06-08 10:11:06 +0800

    更改grunt配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
   // Force use of Unix newlines
   grunt.util.linefeed = '\n';
@@ -6,17 +6,24 @@ module.exports = function(grunt) {
   grunt.file.defaultEncoding = 'utf-8';
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      jquery: {
+        src:'node_modules/jquery/dist/jquery.min.js',
+        dest:'js/jquery.min.js'
+      }
+    },
     connect: {
       server: {
         options: {
           port: 9001,
           base: '.',
-          keepalive:true
+          keepalive: true
         }
       }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  
+
   grunt.registerTask('default', ['connect']);
 };
