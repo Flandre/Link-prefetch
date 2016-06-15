f0ee8576924b9358894fcecafd6fd77da2ab9d2f
Flandre <192488668@qq.com>
2016-06-15 15:36:21 +0800

    rep

--- js/jquery.initPage.js
+++ js/jquery.initPage.js
@@ -50,10 +50,8 @@ function pageClip() {
 /*翻页事件*/
 //向下翻页
 function nextPage() {
-  alert(1);
   //如果下一页没加载完则阻止向下翻页
   if (pageStatus[currentPage + 1]) {
-    alert(2);
     if (pageStatus[currentPage + 1].css && pageStatus[currentPage + 1].js && !pageStatus[currentPage + 1].imgRest) {
       var totalPage = $('section', 'main').length;
       if (currentPage + 1 < totalPage) {
